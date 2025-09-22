54707d150e607394da55a7946405ffea4c256e17
MrMacNode <Matthew.McNabb@gmail.com>
2025-09-22 18:37:36 -0400

    Cleanup

--- TaxcalcApi/Api/Endpoints/MapIncomeTaxEndpoints.cs
+++ TaxcalcApi/Api/Endpoints/MapIncomeTaxEndpoints.cs
@@ -8,18 +8,13 @@ namespace TaxcalcApi.Api.Endpoints
         public static IEndpointRouteBuilder MapIncomeTaxEndpoints(this IEndpointRouteBuilder app)
         {
             app.MapGet("/api/incometax/uk", IncomeTaxHandlers.GetUkIncomeTax)
-            .WithName("GetUkIncomeTax")
-            .WithSummary("Calculates UK Income Tax")
-            .WithDescription("Calculates the UK income tax based on the provided annual salary, with monthly figures.")
-            .WithTags("IncomeTaxUk")
-            .Produces<IncomeTaxResult>(StatusCodes.Status200OK)
-            .Produces(StatusCodes.Status400BadRequest);
-            //.WithOpenApi(op =>
-            //{
-            //    op.Summary = "Calculate UK Income Tax";
-            //    op.Description = "Calculates the UK income tax based on the provided annual salary.";
-            //    return op;
-            //});
+                .WithName("GetUkIncomeTax")
+                .WithSummary("Calculates UK Income Tax")
+                .WithDescription("Calculates the UK income tax based on the provided annual salary, with monthly figures.")
+                .WithTags("IncomeTaxUk")
+                .Produces<IncomeTaxResult>(StatusCodes.Status200OK)
+                .Produces(StatusCodes.Status400BadRequest);
+
             return app;
         }
     }
