4c61f11d2097199ae97fc0acda0d2e97e4839e16
MrMacNode <Matthew.McNabb@gmail.com>
2025-09-22 18:42:27 -0400

    Documented endpoint handler

--- TaxcalcApi/Api/Handlers/IncomeTaxHandlers.cs
+++ TaxcalcApi/Api/Handlers/IncomeTaxHandlers.cs
@@ -7,6 +7,15 @@ namespace TaxcalcApi.Api.Handlers
 {
     public static class IncomeTaxHandlers
     {
+        /// <summary>
+        /// Manages requests to calculate UK income tax based on an annual salary.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="incomeTaxCalculator">Service layer component to perform the calculations.</param>
+        /// <param name="validator">A validator for parameters.</param>
+        /// <param name="parameters">Packaged parameters against which to perform the calculations.</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public static async Task<IResult> GetUkIncomeTax(
             ILogger<IncomeTaxQueryModel> logger,
             IIncomeTaxCalculator incomeTaxCalculator,
@@ -18,6 +27,7 @@ namespace TaxcalcApi.Api.Handlers
             try
             {
                 logger.LogInformation("Received request to calculate UK income tax for annual salary: {AnnualSalary}", parameters.AnnualSalaryString);
+
                 var validationResult = await validator.ValidateAsync(parameters, cancellationToken);
                 if (!validationResult.IsValid)
                 {
@@ -30,6 +40,7 @@ namespace TaxcalcApi.Api.Handlers
                         .ToList();
                     return Results.BadRequest(new { Errors = errors });
                 }
+
                 var result = await incomeTaxCalculator.CalculateUkAnnual(parameters.AnnualSalary, cancellationToken);
                 logger.LogInformation("Successfully calculated UK income tax for annual salary.");
                 return Results.Ok(result);
